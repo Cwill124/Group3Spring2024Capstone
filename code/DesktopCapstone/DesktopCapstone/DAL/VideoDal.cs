399f359c07992ec798871b893602e1916df6bddd
srobles1 <srobles1@my.westga.edu>
2024-01-27 21:45:48 -0500

    remade desktop in wpf and added pdf and video viewing
    The project was in windows form so I remade it in wpf and added pdf and video viewing.

---
+++ code/DesktopCapstone/DesktopCapstone/DAL/VideoDal.cs
@@ -0,0 +1,37 @@
+﻿using desktop_capstone.model;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace desktop_capstone.DAL
+{
+    public class VideoDal
+    {
+
+        public ExternalVideo getSpecifiedVideo(int id)
+        {
+
+
+            var connectionString = Connection.ConnectionString;
+            var query = "select* from capstone.video_link where id = 1";
+
+            using (IDbConnection dbConnection = new NpgsqlConnection(connectionString))
+            {
+                //var connection = new NpgsqlConnection(connectionString);
+                //connection.Open();
+
+                var received = new ExternalVideo();
+                //select* from capstone.app_user where username = @Username and password = @Password
+                var result = dbConnection.QuerySingleOrDefault<ExternalVideo>(query, received);
+
+                return result;
+            }
+        }
+
+    }
+}
---
+++ code/DesktopCapstone/DesktopCapstone/view/Login.xaml.cs
@@ -0,0 +1,62 @@
+﻿using desktop_capstone.DAL;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace desktop_capstone.view
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Login pressed");
+
+            var validLogin = this.handleLogin();
+
+            if (validLogin != null)
+            {
+                MediaViewer newPage = new MediaViewer();
+                //this.Close();
+                newPage.Show();
+                this.Close();
+            }
+        }
+
+        private model.User handleLogin()
+        {
+            var username = this.txtUsername.Text;
+            var password = this.txtPassword.Password;
+
+            var loginResult = new model.User();
+
+            try
+            {
+                loginResult = new LoginDAL().checkLogin(username, password);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("connection failed");
+            }
+            return loginResult;
+        }
+    }
+}
