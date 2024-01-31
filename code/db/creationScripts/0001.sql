2818751a6f93e2860b8fd5d93d93d48eacff7281
Corey Williams <107897988+Cwill124@users.noreply.github.com>
2024-01-30 19:47:48 -0500

    new creation script and removed old useless files

--- code/db/creationScripts/0001.sql
+++ code/db/creationScripts/0001.sql
@@ -1,31 +1,57 @@
+CREATE SCHEMA IF NOT EXISTS capstone;
+
+-- Use the capsone schema
 SET search_path TO capstone;
 
-create table app_user (
-	user_id integer not null GENERATED always as IDENTITY primary key,
-	username varchar(225) not null,
-	password varchar(225) not null,
-	firstname varchar(225),
-	lastname varchar(225),
-	email varchar(225),
-	phone char(10),
-	role varchar(100) not null,
-	created_at timestamp default now() not null
-);
-create table pdf (
-	id integer not null GENERATED always as IDENTITY primary key,
-	name varchar(225) not null,
-	link varchar(500)
-);
-create table video_link (
-	id integer not null GENERATED always as IDENTITY primary key,
-	name varchar(225) not null,
-	link varchar(500)
-);
-INSERT INTO app_user (username, password, firstname, lastname, email, phone, role)
-VALUES ('example_user', 'example_password', 'John', 'Doe', 'john@example.com', '1234567890', 'user');
+-- Drop tables if they exist
+DROP TABLE IF EXISTS login;
+DROP TABLE IF EXISTS app_user;
+DROP TABLE IF EXISTS source;
+DROP TABLE IF EXISTS source_type;
+DROP TABLE IF EXISTS note;
+DROP TABLE IF EXISTS note_comment;
 
-INSERT INTO pdf (name, link)
-VALUES ('example_pdf', 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf');
+-- Create tables
+CREATE TABLE login (
+    login_id SERIAL PRIMARY KEY,
+    username VARCHAR(225) NOT NULL unique,
+    password VARCHAR(225) NOT NULL
+);
 
-INSERT INTO video_link (name, link)
-VALUES ('example_video', 'https://www.youtube.com/watch?v=C0DPdy98e4c');
+CREATE TABLE app_user (
+    username VARCHAR(255) PRIMARY KEY REFERENCES capstone.login(username) not null,
+    fname VARCHAR(255),
+    lname VARCHAR(255),
+    phone CHAR(10),
+    email VARCHAR(255)
+);
+
+CREATE TABLE source_type (
+    source_type_id SERIAL PRIMARY KEY,
+    type_name VARCHAR(225) NOT NULL
+);
+
+CREATE TABLE source (
+    source_id SERIAL PRIMARY KEY,
+    description VARCHAR(600),
+    name VARCHAR(225) NOT NULL,
+    content JSON NOT NULL,
+    author VARCHAR(225),
+    source_type_id INTEGER REFERENCES capstone.source_type(source_type_id) NOT NULL,
+    tags JSON,
+    created_by VARCHAR(225)
+);
+
+CREATE TABLE note (
+    note_id SERIAL PRIMARY KEY,
+    source_id INTEGER REFERENCES capstone.source(source_id) NOT NULL,
+    content JSON NOT NULL,
+    username VARCHAR(225) NOT NULL
+);
+
+CREATE TABLE note_comment (
+    comment_id SERIAL PRIMARY KEY,
+    note_id INTEGER REFERENCES capstone.note(note_id) NOT NULL,
+    source_id INTEGER REFERENCES capstone.source(source_id) NOT NULL,
+    username VARCHAR(225) NOT NULL
+);
