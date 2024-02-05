3b4065b7bd986375389cca1c9ef2acba27c5bb27
Saul Robles <srobles1@my.westga.edu>
2024-02-05 12:17:22 -0500

    Added source object and updated source windows
    Gave definition to source object and added controls to source windows

--- code/DesktopCapstone/DesktopCapstone/model/Source.cs
+++ code/DesktopCapstone/DesktopCapstone/model/Source.cs
@@ -26,6 +26,28 @@ namespace DesktopCapstone.model
         public JsonObject Tags { get { return tags; } }
         public string CreatedBy { get {  return createdBy; } }
     
-    
+        public Source ()
+        {
+            this.description = string.Empty;
+            this.name = string.Empty;
+            this.content = new JsonObject ();
+            this.metaData = new JsonObject ();
+            this.sourceType = 0;
+            this.tags = new JsonObject ();
+            this.createdBy = string.Empty;
+            this.tags = new JsonObject ();
+
+        }
+
+        public Source (string description, string name, JsonObject content, JsonObject metaData, int sourceType, JsonObject tags, string createdBy)
+        {
+            this.description = description;
+            this.name = name;
+            this.content = content;
+            this.metaData = metaData;
+            this.sourceType = sourceType;
+            this.tags = tags;
+            this.createdBy = createdBy;
+        }
     }
 }
