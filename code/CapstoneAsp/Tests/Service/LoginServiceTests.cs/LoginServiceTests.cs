89ef9e997a1937acf402a25eac9ee518b5b8efe5
Corey Williams <107897988+Cwill124@users.noreply.github.com>
2024-02-07 02:15:34 -0500

    (issues #14) created model tests, and class diagram for asp

---
+++ code/CapstoneAsp/Tests/Service/LoginServiceTests.cs/LoginServiceTests.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace CapstoneASP.Tests.Service.LoginServiceTests.cs
+{
+    [ExcludeFromCodeCoverage]
+    public class LoginServiceTests
+    {
+    }
+}
