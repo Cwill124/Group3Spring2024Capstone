54ca09fe7e22b646b822d1585483b400bbadfdba
Corey Williams <coreywilliams3033@yahoo.com>
2024-03-04 13:20:40 -0500

    (issues #36) generate docstrings for tagController and TagService
    Generated docstrings for TagController and TagService to increase Documentation for application
    
    -Corey Williams

--- code/CapstoneAsp/Controllers/TagController.cs
+++ code/CapstoneAsp/Controllers/TagController.cs
@@ -1,36 +1,48 @@
 ﻿using CapstoneASP.Database.Service;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
 
 namespace CapstoneASP.Controllers
 {
+    /// <summary>
+    /// Controller for managing operations related to tags.
+    /// </summary>
     [Route("")]
     [ApiController]
     public class TagController : ControllerBase
     {
-        #region Data member
+        #region Data Members
 
         private readonly IConfiguration config;
-        
         private readonly ITagService tagService;
-
 
         #endregion
 
         #region Constructor
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TagController"/> class.
+        /// </summary>
+        /// <param name="config">The configuration.</param>
+        /// <param name="tagService">The tag service.</param>
         public TagController(IConfiguration config, ITagService tagService)
         {
             this.config = config;
             this.tagService = tagService;
         }
 
-
         #endregion
 
         #region Methods
 
-
+        /// <summary>
+        /// Deletes a tag by its ID.
+        /// </summary>
+        /// <param name="tagId">The ID of the tag to delete.</param>
+        /// <returns>An action result indicating success or failure.</returns>
         [HttpPost]
         [Route("Tags/DeleteById")]
         public async Task<ActionResult> DeleteById([FromBody] int tagId)
@@ -39,7 +51,6 @@ namespace CapstoneASP.Controllers
             {
                 await this.tagService.DeleteTagById(tagId);
                 return Ok("Tag Deleted successfully");
-
             }
             catch (Exception ex)
             {
@@ -47,7 +58,7 @@ namespace CapstoneASP.Controllers
             }
         }
 
-
         #endregion
     }
 }
+
--- code/CapstoneAsp/Database/Service/TagService.cs
+++ code/CapstoneAsp/Database/Service/TagService.cs
@@ -1,27 +1,42 @@
 ﻿using CapstoneASP.Database.Repository;
+using System.Threading.Tasks;
 
 namespace CapstoneASP.Database.Service
 {
-    public interface ITagService {
+    /// <summary>
+    /// Interface for tag-related operations.
+    /// </summary>
+    public interface ITagService
+    {
         #region Methods
 
-        public Task DeleteTagById(int tagId);
+        /// <summary>
+        /// Deletes a tag by its ID asynchronously.
+        /// </summary>
+        /// <param name="tagId">The ID of the tag to delete.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task DeleteTagById(int tagId);
 
         #endregion
     }
+
+    /// <summary>
+    /// Service class for managing tag-related operations.
+    /// </summary>
     public class TagService : ITagService
     {
-
-        #region Data members
+        #region Data Members
 
         private readonly ITagRepository tagRepository;
-
 
         #endregion
 
         #region Constructors
 
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TagService"/> class.
+        /// </summary>
+        /// <param name="tagRepository">The tag repository.</param>
         public TagService(ITagRepository tagRepository)
         {
             this.tagRepository = tagRepository;
@@ -31,12 +46,13 @@ namespace CapstoneASP.Database.Service
 
         #region Methods
 
-         public async Task DeleteTagById(int tagId)
-         {
-             await this.tagRepository.DeleteTagById(tagId);
-         }
+        /// <inheritdoc/>
+        public async Task DeleteTagById(int tagId)
+        {
+            await this.tagRepository.DeleteTagById(tagId);
+        }
 
         #endregion
-        
     }
 }
+
